52bc7579388242e109fb31635e568dd4b29803d3
NetAcad DEVASC <changeme@example.com>
2022-07-03 05:18:58 +0000

    update login notif

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ body_json = {
 }
 
 print(username, password)
+
+respon = requests.post(ticket_url, json.dumps(body_json), headers=headers, verify=False)
+respon_json = respon.json()
+if respon.status_code > 201 :
+    print("Login Fail/Error,",respon_json["response"]["message"])
+    exit()
+
+print("Login Success")
+serviceTicket = respon_json["response"]["serviceTicket"]
+action = 99
+
+headers={"X-Auth-Token": serviceTicket}
