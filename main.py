7115c4eb1570e594f3e720fe63346eb91a55de3c
NetAcad DEVASC <changeme@example.com>
2022-07-03 04:55:26 +0000

    main py ver.1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-Hello world, 123
+import json
+import requests
+
+ticket_url = "http://localhost:58000/api/v1/ticket"
+
+print("Network Programibility: SMKN 13 Bandung")
+username = input("Username: ")
+password = input("Password: ")
+
+headers = {
+    "content-type": "application/json"
+}
+
+body_json = {
+    "username": username,
+    "password": password  
+}
+
+print(username, password)
